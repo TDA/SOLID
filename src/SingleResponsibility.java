cfcff7ba060039734341283ef69cf5051aff458d
Sai Chandramouli <schand31@asu.edu>
2016-12-18 23:44:34 -0800

    single responsibility

--- src/SingleResponsibility.java
+++ src/SingleResponsibility.java
@@ -2,4 +2,8 @@
  * Created by schandramouli on 12/16/16.
  */
 public class SingleResponsibility {
+    // any class should only have one reason to change, i.e. one single responsibility
+    // it should not have any other reason to change, e.g.  a reports class can be
+    // responsible for maintaining reports (storing/retrieving), however, printing or
+    // exporting the reports should be a separate class.
 }
