47a7797be23a2bbea4f843d1507dfb6f501df3b0
Sai Chandramouli <schand31@asu.edu>
2016-12-19 22:16:30 -0800

    liskov

--- src/LiskovSub.java
+++ src/LiskovSub.java
@@ -2,4 +2,7 @@
  * Created by schandramouli on 12/16/16.
  */
 public class LiskovSub {
+    // Liskov substitution says that any function that accepts a base class object
+    // (either a pointer or a reference) should also be able to work with a child
+    // of that class without knowing it.
 }
