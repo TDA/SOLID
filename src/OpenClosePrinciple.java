ae171bb5cb5f1c12b1223e1a10dec783ef29308e
Sai Chandramouli <schand31@asu.edu>
2016-12-20 23:05:08 -0800

    open close

--- src/OpenClosePrinciple.java
+++ src/OpenClosePrinciple.java
@@ -2,4 +2,8 @@
  * Created by schandramouli on 12/16/16.
  */
 public class OpenClosePrinciple {
+    // This states that classes should be open for extension, but closed for modification
+    // basically it means we can extend a class by inheritance, but not modify it, i.e. the classes
+    // should act more like interfaces, and we can then extend their functionality without having to
+    // modify the original class
 }
