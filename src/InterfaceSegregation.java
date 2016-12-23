2cbbe591526ce7ac55e80f6cff15e2b156458aec
Sai Chandramouli <schand31@asu.edu>
2016-12-22 23:31:18 -0800

    interface segregation

--- src/InterfaceSegregation.java
+++ src/InterfaceSegregation.java
@@ -2,4 +2,9 @@
  * Created by schandramouli on 12/16/16.
  */
 public class InterfaceSegregation {
+    // Interface segregation means that interfaces should basically have high cohesion, i.e. they
+    // should only contain methods that are relevant to their clients, formally stated, it says that
+    // no clients should be forced to implement methods that are not relevant to them. If interfaces
+    // have a lot fo methods, they should be broken down into smaller, more cohesive components (in this
+    // way, it is very similar to single responsibility).
 }
